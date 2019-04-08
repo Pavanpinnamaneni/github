489bd8e18a701dafe108c0aebbb1c316ef20e707
Pavanpinnamaneni <35082583+Pavanpinnamaneni@users.noreply.github.com>
2019-04-08 13:34:27 -0700

    python script changes added
    changed in python

--- src/my_square.py
+++ src/my_square.py
@@ -1,5 +1,5 @@
 def my_square(X):
-           """takes the value and gives the squared value.
+           """takes the value and gives the squared values.
            """     
         
         return(X ** 2)
